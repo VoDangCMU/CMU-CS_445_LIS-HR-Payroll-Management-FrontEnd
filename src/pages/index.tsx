9bda459019ca2a4ee35de084a7dd9c038e6696c4
Tran Nguyen Quoc Bao <122146748+tnqbao@users.noreply.github.com>
2025-02-14 10:52:54 +0700

    add daskboard page

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import {AppSidebar} from "@/components/app-sidebar";
+import Page from "@/components/dashboard/page";
+
+const HomePage: React.FC = () => {
+    return <>
+        <Page />
+    </>;
+}
+
+export default HomePage;
